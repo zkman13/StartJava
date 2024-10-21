dc4b7d2f4d8f9c4775433071bb28cf3e1146b115
crzoukman <zoukman@yandex.ru>
2024-10-21 23:52:01 +0300

    done 10.2 guessing game

---
+++ Lesson_1/GuessingGame.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100) + 1;
+
+        System.out.println("Угадайте число от 1 до 100!");
+
+        Scanner scanner = new Scanner(System.in);
+        int parsedRandomNumber = 0;
+
+        while (parsedRandomNumber != randomNumber) {
+            System.out.print("Введите число: ");
+            String input = scanner.nextLine().trim();
+
+            try {
+                parsedRandomNumber = Integer.parseInt(input);
+
+                if (parsedRandomNumber == randomNumber) {
+                    System.out.println("Вы угадали!");
+                } else if (parsedRandomNumber < randomNumber) {
+                    System.out.println("Ваше число меньше чем то, что загадал компьютер!");
+                } else {
+                    System.out.println("Ваше число больше чем то, что загадал компьютер!");
+                }
+            } catch (Exception e) {
+                System.out.println("Вы должны ввести целое число!");
+                System.exit(0);
+            }
+        }
+
+        scanner.close();
+    }
+}
