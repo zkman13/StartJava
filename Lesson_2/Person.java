242da3f47795ae4560ab3446925d652159839d9c
crzoukman <zoukman@yandex.ru>
2024-10-22 14:49:28 +0300

    done 2.2 - Person class

---
+++ Lesson_2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    private String sex = "Male";
+    private String name = "Slacker";
+    private int height = 168;
+    private int weight = 70;
+    private int age = 35;
+
+    public void walk() {
+        System.out.println(name + " идёт.");
+    }
+
+    public void sit() {
+        System.out.println(name + " сидит.");
+    }
+
+    public void run() {
+        System.out.println(name + " бежит.");
+    }
+
+    public void talk() {
+        System.out.println(name + " говорит.");
+    }
+
+    public void learnJava() {
+        System.out.println(name + " учит Java.");
+    }
+}
