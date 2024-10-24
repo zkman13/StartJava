11b3633cbe709f6a192bdbd744768e0062e630a7
crzoukman <zoukman@yandex.ru>
2024-10-24 20:57:53 +0300

    delete something

--- Lesson_2/GuessNumberTest.java
+++
@@ -1,9 +0,0 @@
-package Lesson_2;
-
-public class GuessNumberTest {
-
-    public static void main(String[] args) {
-        GuessNumber guessNumber = new GuessNumber();
-        guessNumber.run();
-    }
-}
