df85ec049ab4911d1705d38c4cb9d70bce62f2d2
crzoukman <zoukman@yandex.ru>
2024-11-20 17:31:07 +0300

    ready to work

--- src/com/startjava/lesson_2_3_4/bookshelf/Book.java
+++ src/com/startjava/lesson_2_3_4/bookshelf/Book.java
@@ -1,6 +1,7 @@
 package com.startjava.lesson_2_3_4.bookshelf;
 
 public class Book extends AbstractBook {
+
     private final String bookInfo;
 
     public Book(final String title, final String author, final int year) {
